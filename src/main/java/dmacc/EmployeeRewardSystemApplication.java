f34ad95767f7846b4cea91ca4cf719f025b66bf5
jleien <jleiendecker@dmacc.com>
2022-03-31 18:12:08 -0500

    initial commit

---
+++ src/main/java/dmacc/EmployeeRewardSystemApplication.java
@@ -0,0 +1,13 @@
+package dmacc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRewardSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRewardSystemApplication.class, args);
+	}
+
+}

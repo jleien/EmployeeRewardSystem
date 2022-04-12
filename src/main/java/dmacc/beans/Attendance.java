071e42f274e3e2f42ccbb8998817b869916b61e1
leogex01 <chris.geralds@gmail.com>
2022-04-12 17:43:00 -0500

    Attendance table creation

---
+++ src/main/java/dmacc/beans/Attendance.java
@@ -0,0 +1,25 @@
+/**
+ * @authorchrisg - Cgeralds
+ * CIS175-Fall2022
+ * Apr 12, 2022
+ */
+package dmacc.beans;
+
+/**
+ * @author chrisg
+ *
+ */
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Embeddable;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Data
+@NoArgsConstructor
+@Embeddable
+public class Attendance {
+	@Autowired 
+	private char attendance;  // Char will be either (y)es or (n)o. Yes also encompassed any type of excused absence. 
+	
+
+}

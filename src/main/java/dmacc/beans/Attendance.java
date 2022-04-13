1030229b9c1b1832fd02cf7cd71bc2fed966da9e
leogex01 <chris.geralds@gmail.com>
2022-04-13 04:10:04 -0500

    Attendance table added clock in/out removed simple yes/no option.

--- src/main/java/dmacc/beans/Attendance.java
+++ src/main/java/dmacc/beans/Attendance.java
@@ -11,15 +11,22 @@ package dmacc.beans;
  */
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import javax.persistence.Embeddable;
+import javax.persistence.ManyToOne;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Data
 @NoArgsConstructor
 @Embeddable
 public class Attendance {
-	@Autowired 
-	private char attendance;  // Char will be either (y)es or (n)o. Yes also encompassed any type of excused absence. 
+	private LocalDate date;
+	private LocalTime clockIn;
+	private LocalTime clockOut;
 	
 
 }
